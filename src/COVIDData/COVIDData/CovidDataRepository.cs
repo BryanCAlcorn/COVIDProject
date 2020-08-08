425f73f39aeac2d5b9be0c4e43a607779e03efa5
Bryan <Bryan.C.Alcorn@gmail.com>
2020-08-08 15:25:31 -0700

    County and State query done.

--- src/COVIDData/COVIDData/CovidDataRepository.cs
+++ src/COVIDData/COVIDData/CovidDataRepository.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.VisualBasic.FileIO;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace COVIDData
@@ -25,11 +23,36 @@ namespace COVIDData
 
             var countyCases = countyRow.ConfirmedCases.Where(kvp => range.Contains(kvp.Key));
 
-            var minCases = countyCases.Min(kvp => kvp.Value);
-            var maxCases = countyCases.Max(kvp => kvp.Value);
-            double averageCases = countyCases.Average(kvp => kvp.Value);
+            var minValue = countyCases.Min(kvp => kvp.Value);
+            var minCases = countyCases.First(kvp => kvp.Value == minValue);
 
-            
+            var maxValue = countyCases.Max(kvp => kvp.Value);
+            var maxCases = countyCases.First(kvp => kvp.Value == maxValue);
+
+            var averageCases = countyCases.Average(kvp => kvp.Value);
+
+            return new CovidQueryResult(county, countyRow.Latitude, countyRow.Longitude,
+                averageCases, minCases.Value, minCases.Key, maxCases.Value, maxCases.Key);
+        }
+
+        public async Task<CovidQueryResult> QueryByState(string state, DateRange range)
+        {
+            var data = await CovidData();
+
+            var stateRows = data.Where(d => string.Equals(d.ProvinceState, state, StringComparison.OrdinalIgnoreCase));
+
+            var stateCases = stateRows.SelectMany(county => county.ConfirmedCases.Where(kvp => range.Contains(kvp.Key)));
+
+            var minValue = stateCases.Min(kvp => kvp.Value);
+            var minCases = stateCases.First(kvp => kvp.Value == minValue);
+
+            var maxValue = stateCases.Max(kvp => kvp.Value);
+            var maxCases = stateCases.First(kvp => kvp.Value == maxValue);
+
+            var averageCases = stateCases.Average(kvp => kvp.Value);
+
+            return new CovidQueryResult(state, string.Empty, string.Empty,
+                averageCases, minCases.Value, minCases.Key, maxCases.Value, maxCases.Key);
         }
 
         private async Task<IList<CovidDataRow>> CovidData()
