030a6bdbc6e4e50268fc5e585513f1402fc4067e
Bryan <Bryan.C.Alcorn@gmail.com>
2020-08-08 13:52:23 -0700

    Add Unit test for Data Fetch

---
+++ src/COVIDApp/COVIDTests/DataFetcherTests.cs
@@ -0,0 +1,27 @@
+using COVIDData;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace COVIDTests
+{
+    [TestClass]
+    public class DataFetcherTests
+    {
+        [TestMethod]
+        public void Test_GetData_VerifyDataParsing()
+        {
+            //Arrange
+            var fetcher = new CovidDataFetcher();
+
+            //Act
+            var data = fetcher.GetData().ConfigureAwait(continueOnCapturedContext: false).GetAwaiter().GetResult();
+
+            //Assert
+            Assert.IsNotNull(data, nameof(data));
+            Assert.AreEqual(3340, data.Count, nameof(data.Count));
+            foreach (var dataRow in data)
+            {
+                Assert.AreEqual(199, dataRow.ConfirmedCases.Count, $"{nameof(CovidDataRow.ConfirmedCases)}.Count");
+            }
+        }
+    }
+}
