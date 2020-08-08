0582cd4741eae711af4407673faaf0a7128263da
Bryan <Bryan.C.Alcorn@gmail.com>
2020-08-08 15:10:10 -0700

    Add query result

---
+++ src/COVIDData/COVIDData/CovidQueryResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace COVIDData
+{
+    public class CovidQueryResult
+    {
+        public string County { get; }
+
+        public string State { get; }
+
+        public string Latitude { get; }
+
+        public string Longitude { get; }
+
+        public double AverageDailyCases { get; }
+
+        public int MinimumCaseCount { get; }
+
+        public DateTime MinimumCaseDate { get; }
+
+        public int MaximumCaseCount { get; }
+
+        public DateTime MaximumCaseDate { get; }
+    }
+}
