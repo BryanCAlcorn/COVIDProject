89bc6779caf4b3d5445fb203c6a2b3f50dcc863f
Bryan <Bryan.C.Alcorn@gmail.com>
2020-08-09 13:36:34 -0700

    Fix up error handling logic a bit to be more consistent.

--- src/COVIDApp/COVIDApp/Controllers/CovidDataController.cs
+++ src/COVIDApp/COVIDApp/Controllers/CovidDataController.cs
@@ -23,7 +23,7 @@ namespace COVIDApp.Controllers
             {
                 var dateRange = new DateRange(startDate, endDate);
 
-                if (!string.IsNullOrEmpty(county))
+                if (!string.IsNullOrEmpty(county) && string.IsNullOrEmpty(state))
                 {
                     return Ok(await _dataRepository.QueryByCounty(county, dateRange));
                 }
@@ -33,7 +33,7 @@ namespace COVIDApp.Controllers
                 }
                 else
                 {
-                    return NotFound();
+                    return BadRequest($"Must use either {nameof(county)} or {nameof(state)}");
                 }
             }
             catch (DatesOutOfRangeException)
@@ -59,7 +59,7 @@ namespace COVIDApp.Controllers
             {
                 var dateRange = new DateRange(startDate, endDate);
 
-                if (!string.IsNullOrEmpty(county))
+                if (!string.IsNullOrEmpty(county) && string.IsNullOrEmpty(state))
                 {
                     return Ok(await _dataRepository.GetDailyBreakdownByCounty(county, dateRange));
                 }
@@ -69,7 +69,7 @@ namespace COVIDApp.Controllers
                 }
                 else
                 {
-                    return NotFound();
+                    return BadRequest($"Must use either {nameof(county)} or {nameof(state)}");
                 }
             }
             catch (DatesOutOfRangeException)
@@ -95,7 +95,7 @@ namespace COVIDApp.Controllers
             {
                 var dateRange = new DateRange(startDate, endDate);
 
-                if (!string.IsNullOrEmpty(county))
+                if (!string.IsNullOrEmpty(county) && string.IsNullOrEmpty(state))
                 {
                     return Ok(await _dataRepository.GetRateOfChangeByCounty(county, dateRange));
                 }
@@ -105,7 +105,7 @@ namespace COVIDApp.Controllers
                 }
                 else
                 {
-                    return NotFound();
+                    return BadRequest($"Must use either {nameof(county)} or {nameof(state)}");
                 }
             }
             catch (DatesOutOfRangeException)
